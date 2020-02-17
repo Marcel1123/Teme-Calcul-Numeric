9bdc80425bfbc925791425454e39e7426a9b5f96
Marcel1123 <48123341+Marcel1123@users.noreply.github.com>
2020-02-17 14:42:46 +0200

    Update tema1.py

--- tema1/tema1.py
+++ tema1/tema1.py
@@ -1,1 +1,19 @@
-print("Ok")
+def problema1():
+	precizie = 1
+	while True:
+		if 1 + pow(10, - precizie) == 1:
+			return precizie - 1
+		else:
+			precizie += 1
+
+
+def problema2():
+	precizie = problema1()
+	if (1.0 + pow(10, -precizie)) + pow(10, -precizie) != 1.0 +  ( pow(10, -precizie) + pow(10, -precizie) ):
+		return True
+	else:
+		return False
+
+
+
+print(problema2())

ef9610b58a8b9463e3f8eaf40c69ef1ce02535af
Marcel1123 <48123341+Marcel1123@users.noreply.github.com>
2020-02-17 16:12:16 +0200

    Update tema1.py

--- tema1/tema1.py
+++ tema1/tema1.py
@@ -15,5 +15,28 @@ def problema2():
 		return False
 
 
+def most_appropriate_power_of_two(number):
+	num_len = len(str(bin(number))) - 2
+	if pow(2, num_len) - number < number - pow(2, num_len - 1):
+		return pow(2, num_len)
+	else:
+		return pow(2, num_len - 1)
 
-print(problema2())
+
+def generate_number(number):
+	start = 0
+	if number <= 4:
+		start = 4
+	else:
+		if number & (number - 1) == 0:
+			start = number
+		else:
+			start = most_appropriate_power_of_two(number)
+	while True:
+		if start % math.log(start, 2) == 0:
+			return start
+		else:
+			start = start * 2
+
+
+print(generate_number(12))

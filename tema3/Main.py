6f9927fee624732c02c4f9b785dda115cf16c30e
Andrei2498 <48515205+Andrei2498@users.noreply.github.com>
2020-02-21 19:37:12 +0200

    Update Main.py
    Fixed efficient matrixes construction function.

--- tema3/Main.py
+++ tema3/Main.py
@@ -1,27 +1,20 @@
 
-def build_efficient_matrixes(txt, matrix):
+def build_efficient_matrixes(txt):
     with open(txt, "r") as file:
         size_a = int(file.readline().split("\n")[0])
-        current_line = 0
+        matrix = [[] for i in range(0, size_a)]
         for line in file:
             line_array = line.split(", ")
             line_array[-1] = line_array[-1].split("\n")[0]
-            # print(line_array[1])
             if line_array == ['']:
                 break
-            if current_line != int(line_array[1]):
-                matrix.append([])
-                current_line = int(line_array[1])
-                matrix[-1].append([float(line_array[0]), int(line_array[2])])
-            else:
-                flag = False
-                for element in matrix[-1]:
-                    if element[1] == int(line_array[2]):
-                        element[0] = element[0] + float(line_array[0])
-                        flag = True
-                        break
-                if not flag:
-                    matrix[-1].append([float(line_array[0]), int(line_array[2])])
+            flag = False
+            for element in matrix[int(line_array[1])]:
+                if element[1] == int(line_array[2]):
+                    element[0] += int(line_array[0])
+                    flag = True
+            if flag is False:
+                matrix[int(line_array[1])].append([float(line_array[0]), int(line_array[2])])
     return matrix, size_a
 
 
@@ -32,13 +25,14 @@ def array_sum(first_list, second_list):
             second_list[i[1]] = second_list.get(i[1]) + i[0]
         else:
             second_list[i[1]] = i[0]
-    return sorted([[v, k] for k, v in second_list.items()], key=lambda x: x[1])
+    return [[v, k] for k, v in second_list.items()]
 
 
 def array_product(first_list, second_list):
     return 0
-new_matrix_A, size_A = build_efficient_matrixes("a.txt", [[]])
-# new_matrix_B, size_B = build_efficient_matrixes("b.txt", [[]])
-# new_matrix_AplusB, size_AplusB = build_efficient_matrixes("aplusb.txt", [[]])
-# new_matrix_AoriB, size_AoriB = build_efficient_matrixes("aorib.txt", [[]])
-# sum_matrix = list(map(array_sum, new_matrix_A, new_matrix_B))
+new_matrix_A, size_A = build_efficient_matrixes("a.txt")
+
+new_matrix_B, size_B = build_efficient_matrixes("b.txt")
+new_matrix_AplusB, size_AplusB = build_efficient_matrixes("aplusb.txt")
+# new_matrix_AoriB, size_AoriB = build_efficient_matrixes("aorib.txt")
+sum_matrix = list(map(array_sum, new_matrix_A, new_matrix_B))

ec6e6cb2d4438df409871e57b4127ce04266805f
Andrei2498 <48515205+Andrei2498@users.noreply.github.com>
2020-02-22 16:40:38 +0200

    Final update Tema 3

--- tema3/Main.py
+++ tema3/Main.py
@@ -1,4 +1,4 @@
-
+import time
 def build_efficient_matrixes(txt):
     with open(txt, "r") as file:
         size_a = int(file.readline().split("\n")[0])
@@ -19,20 +19,56 @@ def build_efficient_matrixes(txt):
 
 
 def array_sum(first_list, second_list):
-    second_list = {v: k for k, v in second_list}
-    for i in first_list:
-        if i[1] in second_list.keys():
-            second_list[i[1]] = second_list.get(i[1]) + i[0]
+    first_list = {v: k for k, v in first_list}
+    for i in second_list:
+        if i[1] in first_list.keys():
+            first_list[i[1]] = first_list.get(i[1]) + i[0]
         else:
-            second_list[i[1]] = i[0]
-    return [[v, k] for k, v in second_list.items()]
+            first_list[i[1]] = i[0]
+    return [[v, k] for k, v in first_list.items()]
 
 
-def array_product(first_list, second_list):
-    return 0
+def sort_matrix(matrix):
+    for i in range(0, len(matrix)):
+        matrix[i] = sorted(matrix[i], key=lambda x: x[1])
+    return matrix
+
+def array_product(first_matrix, second_matrix):
+    result_matrix = []
+    dictionary = {}
+    for a_line in first_matrix:
+        dictionary.clear()
+        for a_colum in a_line:
+            for b_line in second_matrix[a_colum[1]]:
+                if b_line[1] in dictionary.keys():
+                    dictionary[b_line[1]] += b_line[0] * a_colum[0]
+                else:
+                    dictionary[b_line[1]] = b_line[0] * a_colum[0]
+        result_matrix.append([[v, k] for k, v in dictionary.items()])
+    return result_matrix
+
+start = time.time()
+
 new_matrix_A, size_A = build_efficient_matrixes("a.txt")
-
 new_matrix_B, size_B = build_efficient_matrixes("b.txt")
 new_matrix_AplusB, size_AplusB = build_efficient_matrixes("aplusb.txt")
-# new_matrix_AoriB, size_AoriB = build_efficient_matrixes("aorib.txt")
+new_matrix_AoriB, size_AoriB = build_efficient_matrixes("aorib.txt")
 sum_matrix = list(map(array_sum, new_matrix_A, new_matrix_B))
+new_matrix_AplusB = sort_matrix(new_matrix_AplusB)
+sum_matrix = sort_matrix(sum_matrix)
+product_matrix = array_product(new_matrix_A, new_matrix_B)
+product_matrix = sort_matrix(product_matrix)
+new_matrix_AoriB = sort_matrix(new_matrix_AoriB)
+
+if new_matrix_AplusB == sum_matrix:
+    print("Sumele sunt egale")
+else:
+    print("Sumele nu sunt egale")
+
+if new_matrix_AoriB == product_matrix:
+    print("Produsele sunt egale")
+else:
+    print("Produsele nu sunt egale")
+
+end_time = time.time() - start
+print(end_time)

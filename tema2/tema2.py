c8b27bd1f06faeaca6691519d4b6a11fc8d350ea
Marcel1123 <48123341+Marcel1123@users.noreply.github.com>
2020-02-24 18:15:43 +0200

    Update tema2.py

--- tema2/tema2.py
+++ tema2/tema2.py
@@ -1,9 +1,9 @@
 import numpy
 import math
 import random
-import scipy.linalg as la
 
 
+"""
 def precizie_masina():
 	precizie = 1
 	while True:
@@ -17,9 +17,39 @@ def tema2(dimensiune):
 	epsilon = pow(10, - precizie_masina())
 	matrix_A = numpy.array([[random.randint(1, 10) for i in range(0, dimensiune)] for j in range(0, dimensiune)])
 	vector_terminali = numpy.array([random.randint(1,10) for i in range(0, dimensiune)])
-	matrix_L, matrix_U = la.lu(matrix_A)
-	print(matrix_A)
-	print(matrix_L)
-	print(matrix_U)
+	matrix_A = list(numpy.linalg.lu(matrix_A))
+	print("Matrix: ", matrix_A)
+	print(numpy.linalg.det(matrix_A))
+"""
+#print(tema2(10))
+def generate_single_one_matrix(dimension, index):
+	if index == 0:
+		result = [1]
+		for i in range(0, dimension - 1):
+			result.append(0)
+		return result
+	else:
+		result = []
+		for i in range(0, index):
+			result.append(0)
+		result.append(1)
+		for i in range(0, dimension - len(result)):
+			result.append(0)
+		return result
+		
 
-print(tema2(10))
+def inv_function(L_matrix, U_matrix, A_matrix):
+	result = [[] for i in range(0, len(A_matrix))]
+	
+	for i in range(0, len(A_matrix)):
+		y_star = numpy.linalg.solve(L_matrix, generate_single_one_matrix(len(A_matrix), i))
+		x_star = numpy.linalg.solve(U_matrix, y_star)
+		for j in range(0, len(A_matrix)):
+			result[j].append(x_star[j])
+	return result
+
+
+# print(generate_single_one_matrix(10, 2))
+print(inv_function([[1,0,0], [2,1,0], [2,1,1]], 
+					[[2.5, 2, 2], [0, 2, 1], [0,0,1.5]],
+					[[2.5, 2,2], [5,6,5], [5,6,6.5]]))
